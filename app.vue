4805fda4ee278ff01eb65ec2c643c40ffb2392cc
Ufuk Ustali <ustaliufuk73@gmail.com>
2023-11-07 01:37:20 +0100

    better reproduction

--- app.vue
+++ app.vue
@@ -1,18 +1,20 @@
 <script lang="ts" setup>
 const { foo, bar } = $(
-  reactive<{ foo: "bar"; bar: "biz" } | { foo: "baz"; bar: null }>({
+  reactive<{ foo: "bar"; bar: { biz: 3 } } | { foo: "baz"; bar: null }>({
     foo: "bar",
-    bar: "biz",
+    bar: {
+      biz: 3,
+    },
   })
 );
 
 watch($$(bar), () => {
   if (foo === "bar") console.log(bar);
-  //                              ^? const bar: "biz"
+  //                              ^? const bar: { biz: 3; }
 });
 </script>
 
 <template>
-  <div v-if="foo === 'bar'">{{ bar }}</div>
-  <!--                          ^? bar: "biz" | null -->
+  <div v-if="foo === 'bar'">{{ bar.biz }}</div>
+  <!--                          ^? '__VLS_ctx.bar' is possibly 'null' -->
 </template>
